4a90c3ccf093763c15a41f3c9cad800bd03851e9
wasdwasdwa <58357140+wasdwasdwa@users.noreply.github.com>
2023-03-10 23:05:19 +0800

    Update mazeWalk.py

--- mazeWalk.py
+++ mazeWalk.py
@@ -5,16 +5,19 @@ for i in range(1,len(dp)):
 dp[0][0] = arr[0][0]
 m=3
 n=3
-#向下
+#第一列先弄出来
 for x in range(1,n):
     dp[x][0] = min(dp[x][0], arr[x][0] + dp[x - 1][0])
 
+#每一行往右
 for x in range(n):
     curr_x = x
     curr_y = 1
     while curr_y< m:
         dp[curr_x][curr_y] = dp[curr_x][curr_y-1] + arr[curr_x][curr_y]
         curr_y += 1
+        
+#从第二列开始往下
 for y in range(1, m):
     curr_x = 1
     curr_y = y
