ea485cf7be999055e04479ccaef6149539aa158d
jdczf <sc18qg@leeds.ac.uk>
2023-03-10 23:04:06 +0800

    dp exercise

---
+++ mazeWalk.py
@@ -0,0 +1,27 @@
+arr = [[1,3,1],[1,5,1],[4,2,1]]
+dp = arr.copy()
+for i in range(1,len(dp)):
+    dp[i] = [float('inf')]*len(dp[i])
+dp[0][0] = arr[0][0]
+m=3
+n=3
+#向下
+for x in range(1,n):
+    dp[x][0] = min(dp[x][0], arr[x][0] + dp[x - 1][0])
+
+for x in range(n):
+    curr_x = x
+    curr_y = 1
+    while curr_y< m:
+        dp[curr_x][curr_y] = dp[curr_x][curr_y-1] + arr[curr_x][curr_y]
+        curr_y += 1
+for y in range(1, m):
+    curr_x = 1
+    curr_y = y
+    while curr_x<n:
+        dp[curr_x][curr_y] = min(dp[curr_x][curr_y],dp[curr_x-1][curr_y]+arr[curr_x][curr_y])
+        curr_x += 1
+
+print(dp)
+
+
